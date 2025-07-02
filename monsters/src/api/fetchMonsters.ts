97d156e123aa4dbc06617f6189452beb706e4983
Jac <jac.mckeigue@outlook.com>
2025-07-02 22:28:26 +0100

    correct api call so it works with multiple word names

--- monsters/src/api/fetchMonsters.ts
+++ monsters/src/api/fetchMonsters.ts
@@ -1,10 +1,13 @@
 const monstersUrl = "https://www.dnd5eapi.co/api/monsters/";
-
+  
 export const fetchMonster = async (name: string) => {
-    const response = await fetch(`${monstersUrl}${name.toLowerCase()}`);
+    const formattedName = name.toLowerCase().replace(/\s+/g, '-'); // replace spaces with dashes
+    const response = await fetch(`${monstersUrl}${formattedName}`);
+  
     if (!response.ok) {
-      console.log(`Status: ${response.status}`);
-      throw new Error("Oops, that didn't work, sorry.");   
-    }
+        console.log(`Status: ${response.status}`);
+        throw new Error("Oops, that didn't work, sorry.");   
+      }
     return response.json();
   };
+  
